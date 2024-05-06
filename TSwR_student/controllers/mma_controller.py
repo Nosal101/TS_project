75909fdcd285c71de5a0ef9ecebbe5f7b8a59cc8
Krzysztof Nosal <84777767+Nosal101@users.noreply.github.com>
2024-05-06 21:08:53 +0200

    Task_1

--- TSwR_student/controllers/mma_controller.py
+++ TSwR_student/controllers/mma_controller.py
@@ -1,5 +1,6 @@
 import numpy as np
 from .controller import Controller
+from models.manipulator_model import ManiuplatorModel
 
 
 class MMAController(Controller):
@@ -8,18 +9,36 @@ class MMAController(Controller):
         # I:   m3=0.1,  r3=0.05
         # II:  m3=0.01, r3=0.01
         # III: m3=1.0,  r3=0.3
-        self.models = [None, None, None]
+        #self.models = [None, None, None]
+        self.Tp = Tp
         self.i = 0
+        self.models = [
+            self.create_manipulator_model(0.1, 0.05),
+            self.create_manipulator_model(0.01, 0.01),
+            self.create_manipulator_model(1.0, 0.3)]
+
+    def create_manipulator_model(self, m3, r3):
+        manipulator_model = ManiuplatorModel(self.Tp)
+        manipulator_model.set_m3(m3)
+        manipulator_model.set_r3(r3)
+        return manipulator_model
+    
 
     def choose_model(self, x):
         # TODO: Implement procedure of choosing the best fitting model from self.models (by setting self.i)
-        pass
+        differences = [np.linalg.norm(x - model.state) for model in self.models]
+        self.i = np.argmin(differences)
+        print(self.models[self.i].m3)
+        
 
     def calculate_control(self, x, q_r, q_r_dot, q_r_ddot):
         self.choose_model(x)
         q = x[:2]
         q_dot = x[2:]
-        v = q_r_ddot # TODO: add feedback
+        #v = q_r_ddot # TODO: add feedback
+        Kd = 1
+        Kp = 1
+        v = q_r_ddot + Kd*(q_r_dot - q_dot) + Kp*(q_r - q)
         M = self.models[self.i].M(x)
         C = self.models[self.i].C(x)
         u = M @ v[:, np.newaxis] + C @ q_dot[:, np.newaxis]
--- TSwR_student/models/manipulator_model.py
+++ TSwR_student/models/manipulator_model.py
@@ -5,24 +5,56 @@ class ManiuplatorModel:
     def __init__(self, Tp):
         self.Tp = Tp
         self.l1 = 0.5
-        self.r1 = 0.01
-        self.m1 = 1.
-        self.l2 = 0.5
-        self.r2 = 0.01
-        self.m2 = 1.
+        self.r1 = 0.04
+        self.m1 = 3.
+        self.l2 = 0.4
+        self.r2 = 0.04
+        self.m2 = 2.4
         self.I_1 = 1 / 12 * self.m1 * (3 * self.r1 ** 2 + self.l1 ** 2)
         self.I_2 = 1 / 12 * self.m2 * (3 * self.r2 ** 2 + self.l2 ** 2)
-        self.m3 = 0.0
-        self.r3 = 0.01
+        self.m3 = 1.0
+        self.r3 = 0.05
         self.I_3 = 2. / 5 * self.m3 * self.r3 ** 2
 
+        self.d1 = self.l1/2
+        self.d2 = self.l2/2
+
+        self.update_dependent_values()
+        #self.alpha = self.m1*(self.d1**2) + self.I_1 + self.m2*(self.l1**2 + self.d2**2) + self.I_2 
+        #self.betha = self.m2*self.l1*self.d2
+        #self.gama = self.m2*(self.d2**2) + self.I_2
+
+        # self.alpha = self.m1*(self.d1**2) + self.I_1 + self.m2*(self.l1**2 + self.d2**2) + self.I_2 + self.m3*(self.l1**2 + self.l2**2) + self.I_3 
+        # self.betha = self.m2*self.l1*self.d2 + self.m3*self.l1*self.l2
+        # self.gama = self.m2*(self.d2**2) + self.I_2 + self.m3*(self.l2**2) + self.I_3
+
+
+    def update_dependent_values(self):
+        self.alpha = self.m1 * (self.d1 ** 2) + self.I_1 + self.m2 * (self.l1 ** 2 + self.d2 ** 2) + self.I_2 + self.m3 * (self.l1 ** 2 + self.l2 ** 2) + self.I_3
+        self.betha = self.m2 * self.l1 * self.d2 + self.m3 * self.l1 * self.l2
+        self.gama = self.m2 * (self.d2 ** 2) + self.I_2 + self.m3 * (self.l2 ** 2) + self.I_3
+
+    def set_m3(self, m3):
+        self.m3 = m3
+        self.I_3 = 2. / 5 * self.m3 * self.r3 ** 2
+        self.update_dependent_values()
+
+    def set_r3(self, r3):
+        self.r3 = r3
+        self.I_3 = 2. / 5 * self.m3 * self.r3 ** 2
+        self.update_dependent_values()
+        
     def M(self, x):
+
         """
         Please implement the calculation of the mass matrix, according to the model derived in the exercise
         (2DoF planar manipulator with the object at the tip)
         """
         q1, q2, q1_dot, q2_dot = x
-        return NotImplementedError()
+
+        self.macierz = np.array([[self.alpha + 2*self.betha*np.cos(q2), self.gama + self.betha*np.cos(q2)],
+                                 [self.gama + self.betha*np.cos(q2), self.gama]]) 
+        return self.macierz
 
     def C(self, x):
         """
@@ -30,4 +62,7 @@ class ManiuplatorModel:
         in the exercise (2DoF planar manipulator with the object at the tip)
         """
         q1, q2, q1_dot, q2_dot = x
-        return NotImplementedError()
+        
+        self.macierz = np.array([[-1*self.betha*np.sin(q2)*q2_dot, -1*self.betha*np.sin(q2)*(q1_dot+q2_dot)],
+                                 [self.betha*np.sin(q2)*q1_dot, 0]])
+        return self.macierz
